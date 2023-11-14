8f0c57d4ed034a6398ed2ab9e15c0f0f0f246a56
Chris <aguilera.2010@outlook.com>
2023-11-14 12:17:23 -0600

    se añadieron try and catchs

--- Main.cpp
+++ Main.cpp
@@ -8,11 +8,12 @@
 #include "Video.h"
 #include "Pelicula.h"
 
-using namespace std;
+int main()
+{
 
-int main(){
-    // debo de sacar los datos de un archivo de texto
-    // y guardarlos en un vector de objetos de tipo Video
+    // ------------------------------------------
+    // Variables para la ejecucion del programa
+    // ------------------------------------------
 
     bool continuar = true; // bandera para continuar en el menu
     string id; // id del video a calificar
@@ -24,96 +25,150 @@ int main(){
     string linea; // linea del archivo
     ifstream archivo; // archivo de texto
 
-    vector<shared_ptr<Video>> videos;
+    vector<shared_ptr<Video>> videos; // vector inteligente de videos
 
-    archivo.open("peliculas.txt");
-
-    if(archivo.is_open())
+    // try catch para abrir el archivo
+    try
     {
-        while(getline(archivo, linea))
-        { // lee una linea del archivo
-            
+        archivo.open("peliculas.txt"); 
+
+        // si no se pudo abrir el archivo se llama a la excepcion
+        if(!archivo.is_open()){throw runtime_error("No se pudo abrir el archivo");}
+
+        while(getline(archivo, linea)) // lee una linea del archivo
+        {            
             istringstream iss(linea); // separa la linea en palabras
             
+            // variables para guardar los datos de la linea
             string tipo, nombre, genero, id, duracion_str;
             int duracion, numCalificaciones = 0;
             float calificacion = 0; 
 
-            if(!(iss >> tipo >> id >> nombre >> duracion_str >> genero)) // lee los datos de la linea
-            { 
-                
-                cerr << "Error al leer los datos del archivo.\n";
-            }
-
-            else 
+            // try catch para leer los datos de la linea y la conversion de string a int
+            try
             {
+                if(!(iss >> tipo >> id >> nombre >> duracion_str >> genero)) // lee los datos de la linea
+                { 
+                    throw runtime_error("Error al leer los datos del archivo.");
+                }
                 // convierte la duracion de string a int
                 int duracion = stoi(duracion_str);
                 // crea un objeto de tipo Pelicula y lo guarda en el vector inteligente
                 videos.push_back(make_shared<Pelicula>(tipo, nombre, id, genero, duracion, 0, 0.0));
+
+            }
+            // si no se pudo convertir la duracion a int se llama a la excepcion
+            catch(const std::invalid_argument& e)
+            {
+                std::cerr << "Error al convertir la duracion a un numero" << '\n';
+            }
+            // si no se pudo leer la linea se llama a la excepcion
+            catch(const std::exception& e)
+            {
+                std::cerr << "Error al leer los datos" << '\n';
             }
         }
         archivo.close();
     }
-    else // si abrio el archivo fallo
+    // si no se pudo abrir el archivo se llama a la excepcion
+    catch(const std::exception& e)
     {
-        cout << "No se pudo abrir el archivo" << endl;
+        cerr << e.what() << '\n';
     }
 
-    archivo.open("series.txt");
-    
-    if(archivo.is_open())
+    try
     {
-        while(getline(archivo, linea))
-        {
-            istringstream iss(linea);
+        archivo.open("series.txt");
 
+        // si no se pudo abrir el archivo se llama a la excepcion
+        if(!archivo.is_open()){throw runtime_error("No se pudo abrir el archivo");}
+
+        while(getline(archivo, linea)) // lee una linea del archivo
+        {
+            istringstream iss(linea); // separa la linea en palabras
+
+            // variables para guardar los datos de la linea
+            // deberia de cambiar los nombres? 
             string tipo, nombre, genero, id, serie, episodio, duracion_str;
             int duracion, numCalificaciones = 0;
             float calificacion = 0;
 
-            if (!(iss >> tipo >> id >> nombre >> duracion_str >> genero >> serie >> episodio)) 
+            // try catch para leer los datos de la linea y la conversion de string a int
+            try
             {
-                cerr << "Error al leer los datos del archivo.\n";
-            }
-
-            else 
-            {
+                // lee los datos de la linea sino se puede thorow exception
+                if (!(iss >> tipo >> id >> nombre >> duracion_str >> genero >> serie >> episodio)) 
+                {
+                    throw runtime_error("Error al leer los datos del archivo.");
+                }
+                // convierte la duracion de string a int
                 int duracion = stoi(duracion_str);
+                // crea un objeto de tipo Serie y lo guarda en el vector inteligente
                 videos.push_back(make_shared<Serie>(tipo, nombre, duracion, genero, 0, id, 0, serie, episodio));
+            }
+            // si no se pudo convertir la duracion a int se llama a la excepcion
+            catch(const std::invalid_argument& e)
+            {
+                std::cerr << "Error al convertir la duracion a un numero" << '\n';
+            }    
+            // si no se pudo leer la linea se llama a la excepcion
+            catch(const std::exception& e)
+            {
+                std::cerr << "Error al leer los datos" << '\n';
             }
         }
         archivo.close();
     }
-    else { cout << "No se pudo abrir el archivo" << endl; }
-    
+    // si no se pudo abrir el archivo se llama a la excepcion
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    // ------------------------------
+    // Menu
+    // ------------------------------
+
     while (opcion != 0 && continuar)
     {
-        // menu
-        cout << "1. Imprimir el catalogo" << endl;
-        cout << "2. Calificar video" << endl;
-        cout << "3. Imprimir series por calificacion" << endl;
-        cout << "4. Imprimir peliculas por calificacion" << endl;
-        cout << "5. Imprimir series por genero" << endl;
-        cout << "6. Imprimir peliculas por genero" << endl;
-        cout << "0. Salir" << endl;
-        cout << "Opcion: ";
-        cin >> opcion;
-        // switch para las opciones
+        std::cout << "1. Imprimir el catalogo" << std::endl;
+        std::cout << "2. Calificar video" << std::endl;
+        std::cout << "3. Imprimir series por calificacion" << std::endl;
+        std::cout << "4. Imprimir peliculas por calificacion" << std::endl;
+        std::cout << "5. Imprimir series por genero" << std::endl;
+        std::cout << "6. Imprimir peliculas por genero" << std::endl;
+        std::cout << "0. Salir" << std::endl;
+        std::cout << "Opcion: ";
+        std::cin >> opcion;
+
+        // ------------------------------
+        // Switchs para las opciones
+        // ------------------------------
+
         switch (opcion)
         {
+
+        // ------------------------------
+        // Caso 1
+        // ------------------------------
+
         case 1:
             // imprime el catalogo
-            cout << "Catalogo: " << endl;
+            std::cout << "Catalogo: " << std::endl;
             // iterar sobre el vector de videos
-            for(auto& v: videos){cout << *v << endl;}
+            for(auto& v: videos){std::cout << *v << std::endl;}
             break;
+
+        // ------------------------------
+        // Caso 2
+        // ------------------------------
+        
         case 2:
             // califica un video, no importa si es serie o pelicula
-            cout << "Que calificacion quieres darle al video? " << endl;
-            cin >> calif;
-            cout << "Que video quieres calificar? (introduce su id)" << endl;
-            cin >> id;
+            std::cout << "Que calificacion quieres darle al video? " << std::endl;
+            std::cin >> calif;
+            std::cout << "Que video quieres calificar? (introduce su id)" << std::endl;
+            std::cin >> id;
             // iterar sobre el vector de videos
             for(auto& v: videos)
             {
@@ -128,6 +183,11 @@ int main(){
                 else {v->calificarVideo(videos, id, calif);}
             }
             break;
+
+        // ------------------------------
+        // Caso 3
+        // ------------------------------
+
         case 3:
             // imprime series por calificacion
             for( auto& v: videos)
@@ -137,6 +197,11 @@ int main(){
                 if(v->getTipo() == "s"){v->imprimeXcalif(videos, calif);}
             }
             break;
+
+        // ------------------------------
+        // Caso 4
+        // ------------------------------
+
         case 4:
             // imprime peliculas por calificacion
             for( auto& v: videos)
@@ -146,10 +211,15 @@ int main(){
                 if(v->getTipo() == "p"){v->imprimeXcalif(videos, calif);}
             }
             break;
+
+        // ------------------------------
+        // Caso 5
+        // ------------------------------
+
         case 5:
             // imprime series por genero
-            cout << "Que genero te gustaria ver? " << endl;
-            cin >> genero;
+            std::cout << "Que genero te gustaria ver? " << std::endl;
+            std::cin >> genero;
             for( auto& v: videos)
             {
                 // si el tipo del video es serie
@@ -157,10 +227,15 @@ int main(){
                 if(v->getTipo() == "s"){v->imprimeXgenero(videos, genero);}
             }
             break;
+
+        // ------------------------------
+        // Caso 6
+        // ------------------------------
+
         case 6:
             // imprime peliculas por genero
-            cout << "Que genero te gustaria ver? " << endl;
-            cin >> genero;
+            std::cout << "Que genero te gustaria ver? " << std::endl;
+            std::cin >> genero;
             
             for( auto& v: videos)
             {
@@ -169,14 +244,24 @@ int main(){
                 if(v->getTipo() == "p"){v->imprimeXgenero(videos, genero);}
             }
             break;
+
+        // ------------------------------
+        // Caso 0
+        // ------------------------------
+
         case 0:
             // salir
-            cout << "Saliendo..." << endl;
+            std::cout << "Saliendo..." << std::endl;
             continuar = false;
             break;
+
+        // ------------------------------
+        // Caso default
+        // ------------------------------
+        
         default:
             // opcion no valida
-            cout << "Opcion no valida" << endl;
+            std::cout << "Opcion no valida" << std::endl;
             break;
         }
     }
--- Video.cpp
+++ Video.cpp
@@ -6,23 +6,38 @@
 
 using namespace std;
 
-//constructor default inicializado con el constructor con parametros
+// -----------------------------------------------------------------
+// Constructor default inicializa los atributos con 
+// valores por default pasados con ayuda del constructor 
+// con parametros
+// -----------------------------------------------------------------
+
 Video::Video():Video("v","","","",0,0,0.0){}
-// constructor con parametros
+
+// ------------------------------
+// Constructor con parametros
+// ------------------------------
+
 Video::Video(string tipo, string nombre, string id, string genero, int duracion, int numCalificaciones, float calificacion)
 {
+    // inicializamos los atributos
     this->tipo=tipo;
     this->nombre=nombre;
     this->id=id;
     this->genero=genero;
     this->duracion=duracion;
-    this->numCalificaciones=0;
-    this->calificacion=0.0;
+    this->numCalificaciones = 0;
+    // utilizamos set para validar la calificacion
+    setCalificacion(calificacion);
 }
-// bloque de setters
+
+// ----------------------------------
+// Sett para validar la calificacion
+// ----------------------------------
+
 void Video::setCalificacion(float calificacion){
     // verificamos que la calificacion este entre 0 y 5
-    if(0<=calificacion<=5)
+    if(calificacion >= 0 && calificacion <= 5)
     {
         this->calificacion=calificacion;
     }
@@ -32,44 +47,65 @@ void Video::setCalificacion(float calificacion){
         cout<<"Ingresa un valor valido"<<endl;
     }
 }
-// metodos de la clase
+
+// ------------------------------
+// Metodos virtuales
+// ------------------------------
+
+// metodo para imprimir videos por genero
 void Video::imprimeXgenero(vector<shared_ptr<Video>> &v, string &genero){
+    // recorremos el vector
     for (auto& video : v)
     {
+        // si el genero del video es igual al genero que se busca
         if (video->getGenero()==genero)
         {
+            // se imprime el video con ayuda del operador sobrecargado
             cout<<*video<<endl;
         }
     }
 }
 
-void Video::imprimeXcalif(vector<shared_ptr<Video>> &v, float &calif){
+// metodo para imprimir videos por calificacion
+void Video::imprimeXcalif(vector<shared_ptr<Video>> &v, float &calif){ 
+    // recorremos el vector
     for (auto& video : v)
     {
+        // si la calificacion del video es igual a la calificacion que se busca
         if (video->getCalificacion()==calif)
         {
+            // se imprime el video con ayuda del operador sobrecargado
             cout<<*video<<endl;
         }
     }
 }
 
+// metodo para calificar videos
 void Video::calificarVideo(vector<shared_ptr<Video>> &v, string &id, float &calificacion){
+    // se vuelve a validar la calificacion
     if (calificacion >= 1 && calificacion <= 5)
     {
+        // recorremos el vector
         for (auto& i : v)
         {
+            // si el id del video es igual al id que se busca
             if(i->id==id)
             {
-                int califPromedio=i->calificacion;
-                int cont=i->numCalificaciones;
-                float promedio= round((califPromedio*cont+calificacion)/(cont+1));
+                // se calcula el promedio de la calificacion
+                int califPromedio = i->calificacion; 
+                int cont = i->numCalificaciones;
+                // se redondea el promedio
+                float promedio = round((califPromedio*cont+calificacion)/(cont+1));
                 
+                // se asigna el promedio al video
                 i->calificacion=promedio;
+                // se aumenta el numero de calificaciones
                 i->numCalificaciones++;
                 return;
             }
             else
             {
+                // si no se encuentra el id el video no existe
                 cout<<"Id no encontrado"<<endl;
                 break;
             }
@@ -78,7 +114,12 @@ void Video::calificarVideo(vector<shared_ptr<Video>> &v, string &id, float &cali
     cout << "valor invalido" << endl;
 }
 
+// ------------------------------
+// Sobrecarga de operador
+// ------------------------------
+
 ostream &operator<<(ostream &os, Video &v){
+    // se imprime el video
     os<<v.id<<","<<v.nombre<<","<<v.duracion<<","<<v.genero<<","<<v.calificacion;
-    return os;
+    return os; // se regresa el objeto ostream
 }
